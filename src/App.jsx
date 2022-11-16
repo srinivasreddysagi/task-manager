08c5fff5d7a5efcbda42bf6110060d7e41e5ab2d
Nivas Reddy <srinivasreddysagi@gmail.com>
2022-11-16 21:02:06 +0530

    Connected navbar and redux state

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/navbar";
 import Home from "./components/home";
+import Login from "./components/login";
 import "./App.css";
 import { useSelector } from "react-redux";
 // import { login } from "./store/features/loginData";
@@ -14,7 +15,8 @@ function App() {
                 type={loginDetails.type}
                 username={loginDetails.username}
             />
-            <Home />
+            {/* <Home /> */}
+            <Login />
         </>
     );
 }
--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,7 +1,58 @@
 import React from "react";
+import { useState } from "react";
+import "./styles.css";
+import { useDispatch } from "react-redux";
+import { login } from "../../store/features/loginData";
 
 function Login() {
-    return <></>;
+    const dispatch = useDispatch();
+    const [loginCreds, setLoginCreds] = useState({ email: "", pword: "" });
+
+    function changeHandler(e) {
+        setLoginCreds({ ...loginCreds, [e.target.name]: e.target.value });
+    }
+
+    function submitHandler(e) {
+        e.preventDefault();
+        if (
+            loginCreds.email === "nivasreddy@gmail.com" &&
+            loginCreds.pword === "123456"
+        ) {
+            dispatch(login());
+            setLoginCreds({ email: "", pword: "" });
+        }
+    }
+
+    return (
+        <form
+            action=""
+            method="post"
+            className="login-form"
+            onSubmit={submitHandler}
+        >
+            <div className="login-form__input email">
+                <label htmlFor="email">Email</label>
+                <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    value={loginCreds.email}
+                    onChange={changeHandler}
+                />
+            </div>
+            <div className="login-form__input password">
+                <label htmlFor="pword">Password</label>
+                <input
+                    type="password"
+                    name="pword"
+                    id="pword"
+                    value={loginCreds.pword}
+                    onChange={changeHandler}
+                />
+            </div>
+            <input type="submit" value="Login" />
+        </form>
+    );
 }
 
 export default Login;
